19550b0f2863ad2f868b3854c7cea7df64b55cca
saiteja-yernagula <saiteja.yernagula@gmail.com>
2026-07-16 09:40:47 +0530

    sfdf

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,21 @@ import Section from './components/Section'
 import './components/styles.css'
 import Doctorcard from './components/Doctorcard'
 import Addnewdoctor from './components/Addnewdoctor'
-// import { useState } from 'react'
-
+import { useState } from 'react'
+import { useEffect } from 'react'
 
 function App() {
-  // const [count,setCount]=useState(0);
-  // let count=0
-  // function inc(){
-  //   count++
-  //   console.log(count)
-  // }
-  //   function dnc(){
-  //   count--
-  //   console.log(count)
-
-  // }
-
+  const [count,setCount]=useState(0);
+  const [val,setVal]=useState(0)
+  useEffect(()=>{
+    console.log('request is senting')
+  },[val])
   return (
    <> 
    <br />
-   {/* {count}
+   {count}
+   <button onClick={()=>setVal(val+1)}>value increment</button>
    <button onClick={()=>setCount(count+1)}>increment</button>
-   <button onClick={()=>setCount(count-1)}>decrement</button> */}
    <Navbar/>
    <Section/>
 
