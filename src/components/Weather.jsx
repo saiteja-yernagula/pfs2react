71a3d0c30e333ad0de61bed86902825dab78f3f3
saiteja-yernagula <saiteja.yernagula@gmail.com>
2026-07-20 12:42:55 +0530

    dsf

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,12 +1,18 @@
+import axios from 'axios'
 import React from 'react'
 import { useState } from 'react'
 
 function Weather() {
     const [city,setCity]=useState([])
-    function handleform(e){
+    const [weather,setWeather]=useState([])
+    const [temp,setTemp]=useState('')
+ async  function handleform(e){
         e.preventDefault()
         console.log(city)
-        api='https://api.openweathermap.org/data/2.5/weather?q=egg&units=metric&appid=466ddaa21a8de191e9f608bd11a56acb'
+       let api=`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=466ddaa21a8de191e9f608bd11a56acb`
+       let weatherdata=await axios.get(api)
+       setWeather(weatherdata.data)
+       setTemp(weatherdata.data.main.temp)
     }
   return (
     <div>
@@ -14,6 +20,7 @@ function Weather() {
             <input onChange={e=>setCity(e.target.value)} value={city} type="text" className='text-field' placeholder='enter city'/>
             <button type='submit'>Search</button>
         </form>
+        {weather ? <h1>Temparature is {temp}</h1>:<>no data</>}
 
     </div>
   )
